dd59463f71063a035d76cbbfeb044932b9a01a8c
Stars-Star <45808775+Stars-Star@users.noreply.github.com>
2019-05-21 13:30:15 +0800

    1
    1

--- src/com/neuedu/QA/dao/impl/CommentDaoImpl.java
+++ src/com/neuedu/QA/dao/impl/CommentDaoImpl.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 package com.neuedu.QA.dao.impl;
 
@@ -51,3 +52,69 @@ public class CommentDaoImpl implements CommentDao{
 	}
 
 }
+=======
+package com.neuedu.QA.dao.impl;
+
+import java.sql.ResultSet;
+
+import com.neuedu.QA.dao.CommentDao;
+import com.neuedu.QA.dbutil.BaseDao;
+import com.neuedu.QA.entity.Comment;
+
+public class CommentDaoImpl extends BaseDao implements CommentDao{
+
+	@Override
+	public ResultSet SelectComment(int answer_id, int start, int end) {
+		
+		Object[] params =new Object[]{answer_id,start,end-start};
+		
+		try {
+			
+			ResultSet rs = super.executeSelect("select * from comment where answer_id=? LIMIT ?,?", params);
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			super.closeAll(BaseDao.con, BaseDao.pst, rs);
+		}
+		return rs;
+	}
+
+	@Override
+	public ResultSet SelectSecondComment(int[] secondComment_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultSet SelectUserComment(int User_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Comment SelectOneComment(int comment_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int addComment(Comment d) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateComment(Comment d) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteComment(int Comment_id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
+>>>>>>> f88d9dc8bf6b7bfd82ee883a7e1f234cf9967b0b
