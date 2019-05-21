e14fe6979c7b692a4da3fe722874d1a54cd22343
Stars-Star <45808775+Stars-Star@users.noreply.github.com>
2019-05-21 10:26:50 +0800

    1
    1

--- src/com/neuedu/QA/service/impl/FollwServiceImpl.java
+++ src/com/neuedu/QA/service/impl/FollwServiceImpl.java
@@ -5,6 +5,8 @@ package com.neuedu.QA.service.impl;
 
 import java.util.ArrayList;
 
+import com.neuedu.QA.dao.impl.CollectionInfoDaoImpl;
+import com.neuedu.QA.dao.impl.UserFollowDaoImpl;
 import com.neuedu.QA.entity.UserFollow;
 import com.neuedu.QA.service.FollowService;
 
@@ -14,9 +16,18 @@ import com.neuedu.QA.service.FollowService;
  */
 
 public class FollwServiceImpl implements FollowService {
+	private CollectionInfoDaoImpl collectionInfoDaoImpl =new CollectionInfoDaoImpl() ;
+	private UserFollowDaoImpl attentionInfoDaoImpl =UserFollowDaoImpl();
 	@Override
 	public int AddFollow(int user_id,int to_user_id) {
 		return 0;
+	}
+	/**
+	 * @return
+	 */
+	private UserFollowDaoImpl UserFollowDaoImpl() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	@Override
 	public ArrayList<UserFollow>  ShowFollow(int user_id,int start,int end){
