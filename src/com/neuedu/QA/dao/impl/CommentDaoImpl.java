5e6615f525f5c39173328a03e7c4106aed1d6541
CangDog <37903864+CangDog@users.noreply.github.com>
2019-05-21 13:46:47 +0800

    1
    1

--- src/com/neuedu/QA/dao/impl/CommentDaoImpl.java
+++ src/com/neuedu/QA/dao/impl/CommentDaoImpl.java
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-
-package com.neuedu.QA.dao.impl;
-
-import java.sql.ResultSet;
-
-import com.neuedu.QA.dao.CommentDao;
-import com.neuedu.QA.entity.Comment;
-
-public class CommentDaoImpl implements CommentDao{
-
-	@Override
-	public ResultSet SelectComment(int answer_id, int start, int end) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResultSet SelectSecondComment(int[] secondComment_id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ResultSet SelectUserComment(int User_id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Comment SelectOneComment(int comment_id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int addComment(Comment d) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int updateComment(Comment d) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int deleteComment(int Comment_id) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-}
-=======
 package com.neuedu.QA.dao.impl;
 
 import java.sql.ResultSet;
@@ -117,4 +62,4 @@ public class CommentDaoImpl extends BaseDao implements CommentDao{
 	}
 
 }
->>>>>>> f88d9dc8bf6b7bfd82ee883a7e1f234cf9967b0b
+
